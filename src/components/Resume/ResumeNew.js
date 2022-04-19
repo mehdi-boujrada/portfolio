97ca6c5516394e97df0acb9acbd9a4b706601f89
mehdi-boujrada <boujradamehdi0@gmail.com>
2022-04-19 16:21:22 +0000

    changing cv

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,15 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-// import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
-import pdf from "../../Assets/../Assets/Mehdi-BOUJRADA.pdf";
+import pdf from "../../Assets/../Assets/Mehdi_BOUJRADA.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/mehdi-boujrada/portfolio/master/src/Assets/Mehdi-BOUJRADA.pdf";
+  "https://raw.githubusercontent.com/mehdi-boujrada/portfolio/master/src/Assets/Mehdi_BOUJRADA.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
